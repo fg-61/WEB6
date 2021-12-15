6e1d983acecfa7f264fc1a2a1c338f1153ce58ad
fg-61 <fatihgoren61@gmail.com>
2021-12-15 10:10:58 +0300

    first commit

---
+++ GUN 2/app.js
@@ -0,0 +1,25 @@
+// değişkenler
+
+var degisken1 = 123;
+degisken1 = "Kml";
+degisken1 = 3.14;
+degisken1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+degisken1 = {
+    ad: "Kamil",
+    soyad: "Fıdıl",
+    yas: 43
+};
+degisken1.telefon = "0515125125";
+console.log(degisken1);
+if (degisken1.yas > 40) {
+    console.log("yasş 40 dan büyüktür");
+} else {
+    console.log("yaşı 40 dan küçük");
+}
+
+var degisken2 = 5;
+if (degisken2 === "5") {
+    console.log("5 e esittir");
+} else {
+    console.log("5e esit degil");
+}
